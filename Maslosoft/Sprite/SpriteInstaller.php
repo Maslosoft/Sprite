5a0760f32cf94d30890907d18105013bd59ead41
Peter Maselkowski <pmaselkowski@gmail.com>
2014-05-21 18:25:26 +0200

    + installer

---
+++ Maslosoft/Sprite/SpriteInstaller.php
@@ -0,0 +1,29 @@
+<?php
+
+/**
+ * @licence For licence @see LICENCE.html
+ * 
+ * @copyright Maslosoft
+ * @link http://maslosoft.com/
+ */
+
+namespace Maslosoft\Sprite;
+
+use Maslosoft\Components\Install\InstallersBase;
+
+/**
+ * Sprite installer
+ * @SignalFor('Maslosoft\Components\Install\Installer')
+ * @author Piotr Maselkowski <pmaselkowski at gmail.com>
+ */
+class SpriteInstaller extends InstallersBase
+{
+
+	public function install()
+	{
+		$sprite = new Generator();
+		$sprite->reset();
+		$this->installer->success(tx('Rebuild sprite', 'Maslosoft.Sprite.Installer'));
+	}
+
+}
